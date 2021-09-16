ef7f03d0f6b8fab8224b0f8350bc0e4acbb1bb2b
AdrianVega96 <adrianvega96@gmail.com>
2021-09-16 19:39:40 +0100

    Correciones en ListaPedidos.jsx

--- Frontend/shop/src/components/pedidos/ListaPedidos.jsx
+++ Frontend/shop/src/components/pedidos/ListaPedidos.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import "./ListaPedidos.css";
 import axios from 'axios';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router';
 
 const ListaPedidos = (props) => {
+    const history = useHistory();
     const userData = props.userData;
     const userToken = props.userToken;
 
