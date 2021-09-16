cf1a9472728628b9eb4fb390aa7f70d8502085df
AdrianVega96 <adrianvega96@gmail.com>
2021-09-16 19:50:02 +0100

    Avances en la lista de pedidos - falta crear el componente pedido

--- Frontend/shop/src/components/pedidos/ListaPedidos.jsx
+++ Frontend/shop/src/components/pedidos/ListaPedidos.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
+import React from "react";
 import "./ListaPedidos.css";
-import axios from 'axios';
-import { useEffect } from 'react';
-import { useHistory } from 'react-router';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router";
 
 const ListaPedidos = (props) => {
-    const history = useHistory();
-    const userData = props.userData;
-    const userToken = props.userToken;
+  const history = useHistory();
+  const userData = props.userData;
+  const userToken = props.userToken;
+  const [pedidos, setPedidos] = useState([]);
 
-    const getUserOrders = async () => {
-        axios.get(`http://localhost:5000/shop/orders/user/${userData.userId}`);
+  const getUserOrders = async () => {
+    axios
+      .get(`http://localhost:5000/shop/orders/user/${userData.userId}`, {
+        headers: {
+          Authorization: "Bearer " + userToken,
+        },
+      })
+      .then((datos) => console.log(datos.data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    if (userData === "") {
+      history.push("/");
     }
+    getUserOrders();
+  }, []);
+  return (
+    <div className="ListaPedidos">
+      {pedidos.map((pedido) => {
+        return <Pedido />;
+      })}
+    </div>
+  );
+};
 
-    useEffect(() => {
-        if (userData===""){
-            history.push("/");
-        }
-    }, []);
-    return (
-        <div className="ListaPedidos">
-            
-        </div>
-    )
-}
+const Pedido = (props) => {
+  return <div className="Pedido"></div>;
+};
 
-export default ListaPedidos
+export default ListaPedidos;
