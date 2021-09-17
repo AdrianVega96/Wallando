c2b2c21e8774a0ef9011e7b7a27aa219379d9905
AdrianVega96 <adrianvega96@gmail.com>
2021-09-17 17:14:48 +0100

    Avances en ListaPedidos con material-ui

--- Frontend/shop/src/components/usuario/Profile.jsx
+++ Frontend/shop/src/components/usuario/Profile.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
+import React from "react";
 import "./Profile.css";
-import { useHistory } from 'react-router';
-import { useEffect } from 'react';
+import { useHistory } from "react-router";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Profile = (props) => {
-    const handleLogout = props.handleLogout;
-    const userData = props.userData;
-    const userToken = props.userToken;
-    const history = useHistory();
-    const logout = () => {
-        handleLogout();
-        history.push("/");
+  const handleLogout = props.handleLogout;
+  const userData = props.userData;
+  const userToken = props.userToken;
+  const history = useHistory();
+  const logout = () => {
+    handleLogout();
+    history.push("/");
+  };
+
+  useEffect(() => {
+    const recoverData = JSON.parse(localStorage.getItem("userData"));
+    if (!recoverData) {
+      history.push("/");
     }
+  }, []);
+  return (
+    <div className="Profile">
+      <Link to="/pedidos">
+        <button className="btn btn-primary btn-lg btn-block">
+          Lista de pedidos
+        </button>
+      </Link>
+      <button className="btn btn-danger" onClick={logout}>
+        <i class="fas fa-sign-out-alt"></i>
+      </button>
+    </div>
+  );
+};
 
-    useEffect(() => {
-        const recoverData = JSON.parse(localStorage.getItem("userData"));
-        if (!recoverData) {
-            history.push("/");
-        }
-    }, []);
-    return (
-        <div className="Profile">
-            <button onClick={logout}>Desconectarse</button>
-        </div>
-    )
-}
-
-export default Profile
+export default Profile;
